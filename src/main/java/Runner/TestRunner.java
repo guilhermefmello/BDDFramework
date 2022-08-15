7378e186744f6356c7356a945bdc050e5e0ca69f
Guilherme Ferreira Mello <guilhermefmello@hotmail.com>
2022-08-15 12:30:10 +0100

    adding more attributes into Cucumber Options

--- src/main/java/Runner/TestRunner.java
+++ src/main/java/Runner/TestRunner.java
@@ -9,9 +9,11 @@ import io.cucumber.junit.Cucumber;
 @CucumberOptions(
 		features = "C:\\Users\\Guilherme\\eclipse-workspace\\BDDFramework\\src\\main\\java\\Features\\login.feature",
 		glue= {"stepDefinitions"},
-		stepNotifications = true,
+		stepNotifications = true, //shows the test scenarios steps into JUnit test results tab    
+		monochrome = true, //display the console output in a readable format
 		tags = "@tag1",
-		plugin = {"pretty", "html:target/cucumber-reports" }
+		plugin = {"pretty", "html:target/cucumber-reports" }, //generates different types of reporting
+		dryRun = false //check the mapping between Feature File and Step Definitions file
 	
 		)
 
